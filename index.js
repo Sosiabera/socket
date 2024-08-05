5ba896ad907fe44f73b96a4490579ee78a3e5da7
sosiabera <sosiabera32@gmail.com>
2024-08-05 13:14:09 -0300

    a

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
-const io = require("socket.io")(4100, {
+const io = require('socket.io')(4100, {
   cors: {
-    origin: "http://localhost:5173",
+      origin: 'http://localhost:5173',
   },
 });
 
-let user = [];
+let users = [];
 
 const addUser = (userId, socketId) => {
-  !user.some((user) => user.userId === userId) &&
-    user.push({ userId, socketId });
+  if (!users.some((user) => user.userId === userId)) {
+      users.push({ userId, socketId });
+  }
 };
 
 const getUser = (userId) => {
-  return user.find((user) => user.userId === userId);
+  return users.find((user) => user.userId === userId);
 };
 
 const removeUser = (socketId) => {
-  user = user.filter((user) => user.socketId !== socketId);
+  users = users.filter((user) => user.socketId !== socketId);
 };
 
-io.on("connection", (socket) => {
-  console.log("user connected");
+io.on('connection', (socket) => {
+  console.log('User connected');
 
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", user);
+  socket.on('addUser', (userId) => {
+      addUser(userId, socket.id);
+      io.emit('getUsers', users);
   });
 
-     socket.on("sendMessage", ({senderId, receiverId, text}) => {
-        const user = getUser(receiverId)
-        io.to(user.socketId).emit("getMessage", {
-            senderId, text
-        })
-    })
-
-  socket.on("addUserV", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", user);
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+      const user = getUser(receiverId);
+      if (user) {
+          io.to(user.socketId).emit('getMessage', {
+              senderId,
+              text,
+          });
+      }
   });
-
 
   socket.emit('me', socket.id);
 
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
-    });
-    console.log(data.userToCall)
+  socket.on('callUser', (data) => {
+      io.to(data.userToCall).emit('callUser', {
+          signal: data.signalData,
+          from: data.from,
+          name: data.name,
+      });
+      console.log(`Calling user: ${data.userToCall}`);
   });
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-    console.log(data.to)
+  socket.on('answerCall', (data) => {
+      io.to(data.to).emit('callAccepted', data.signal);
+      console.log(`Call answered by: ${data.to}`);
   });
 
-  socket.on("disconnect-video", () => {
-    socket.broadcast.emit("callEnded");
+  socket.on('sendCandidate', (data) => {
+      io.to(data.to).emit('receiveCandidate', data.candidate);
   });
 
-  socket.on("disconnect", () => {
-    removeUser(socket.id);
-    io.emit("getUsers", user);
+  socket.on('disconnect-video', () => {
+      socket.broadcast.emit('callEnded');
+  });
+
+  socket.on('disconnect', () => {
+      removeUser(socket.id);
+      io.emit('getUsers', users);
+      console.log('User disconnected');
   });
 });
